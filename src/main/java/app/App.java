7c4008e48e85a7669e89e94107b43891a5968c72
luzzambrano <luz.zambrano24@gmail.com>
2023-10-07 13:58:30 -0600

    Aggregar Clase de doctores

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,13 +1,25 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Hello world!
- *
  */
 public class App 
 {
+    private static ArrayList<Administrador> administradores = new ArrayList<>();
+    private static Scanner teclado = new Scanner(System.in);
+    static GestorPacientes pacientes = new GestorPacientes();
+    static GestorCita citas = new GestorCita();
+    static GestorDoctor doctores = new GestorDoctor();
+
+    static void crearAdmin (){
+    }
+
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
     }
 }
---
+++ src/main/java/app/GestorPacientes.java
@@ -0,0 +1,13 @@
+package app;
+
+public class GestorPacientes {
+    class Paciente{
+        public String identificadorUnico;
+        public String nombre;
+
+        Paciente(String identificadorUnico, String nombre) {
+            this.identificadorUnico = identificadorUnico;
+            this.nombre = nombre;
+        }
+    }
+}
