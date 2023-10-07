8086b985d1ad747aa4959ffb2574610266221d9d
luzzambrano <luz.zambrano24@gmail.com>
2023-10-07 14:15:09 -0600

    Aggregar Clase de pacientes

--- src/main/java/app/GestorPacientes.java
+++ src/main/java/app/GestorPacientes.java
@@ -1,5 +1,9 @@
 package app;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class GestorPacientes {
     class Paciente{
         public String identificadorUnico;
@@ -17,5 +21,30 @@ public class GestorPacientes {
         public String toString(){
             return String.format("{Identificador: %s, Nombre: %s}", identificadorUnico, nombre);
         }
+    }
+
+    private Map<String, Paciente> pacientes = new HashMap<>();
+    private Scanner scanner = new Scanner(System.in);
+
+    public void nuevoPaciente(){
+        System.out.print("Nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.print("Identificador: ");
+        String identificadorUnico = scanner.nextLine();
+
+        pacientes.put(identificadorUnico, new Paciente(nombre, identificadorUnico));
+    }
+
+    public void modificarPaciente(){
+        System.out.print("Identificador: ");
+        String identificadorUnico = scanner.nextLine();
+        Paciente paciente = pacientes.get(identificadorUnico);
+        System.out.print("Nombre: ");
+        String nombre = scanner.nextLine();
+        paciente.modificar(nombre);
+    }
+
+    public void mostrarPacientes(){
+        pacientes.forEach((id, paciente) -> paciente.imprimir());
     }
 }
